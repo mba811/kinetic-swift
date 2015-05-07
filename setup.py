f342727cf93c70d89beeadb1fe68914b7ab7312b
Clay Gerrard <clay.gerrard@gmail.com>
2015-05-07 15:46:28 -0700

    initial packaging

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='kinetic-swift',
-    version='0.1',
+    version='0.2',
     description='Kinetic Plugin for Swift',
     author='SwiftStack/Seagate',
     packages=find_packages(),
